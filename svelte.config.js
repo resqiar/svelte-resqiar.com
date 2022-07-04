91d31d8d77db8769407722f1f479ac9d6158a8ed
resqiar <resqiageng@gmail.com>
2022-07-05 00:26:19 +0700

    chore: configure svelte-image package

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,26 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+// @ts-ignore
+import image from 'svelte-image';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true,
-	}),
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+		image({
+			compressionLevel: 7,
+			webpOptions: {
+				quality: 70,
+				lossless: false,
+				force: true,
+			},
+			outputDir: 'generated/',
+		}),
+	],
 
 	kit: {
 		adapter: adapter(),
