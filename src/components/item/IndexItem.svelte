86ad9ba657aac658766d56b426825c8d6b660544
resqiar <resqiageng@gmail.com>
2022-07-06 22:05:59 +0700

    style(index): adjust style for index page

--- src/components/item/IndexItem.svelte
+++ src/components/item/IndexItem.svelte
@@ -1,4 +1,4 @@
-<div class="flex h-full flex-col">
+<div class="flex h-full flex-col rounded-lg bg-white p-4">
 	<img
 		class="flex-grow rounded-lg object-cover"
 		src="/images/background/background-mockup-1.png"
--- src/components/list/IndexGridList.svelte
+++ src/components/list/IndexGridList.svelte
@@ -5,13 +5,13 @@
 <div class="grid gap-2 md:grid-cols-4">
 	<!-- MOST FEATURED ITEM -->
 	<!-- THIS ITEM WILL TAKE 2 ROW IN MEDIUM ++ DEVICES -->
-	<div class="row-span-2 py-2 px-4">
+	<div class="row-span-2 py-2">
 		<IndexItem />
 	</div>
 
 	<!-- THE REST ITEM -->
 	{#each [...new Array(6)] as _value}
-		<div class="py-2 px-4">
+		<div class="py-2 px-2">
 			<IndexItem />
 		</div>
 	{/each}
