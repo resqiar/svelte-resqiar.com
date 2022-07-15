14cabcc08e40a25e3b5149d1732d318fd3746f65
resqiar <resqiageng@gmail.com>
2022-07-16 04:14:14 +0700

    feat(SearchInput): add a function to trigger input focus outside the component

--- src/components/search/SearchInput.svelte
+++ src/components/search/SearchInput.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	export let extend: string = '';
+
+	// Reference (the same as react called useRef)
+	let inputRef: HTMLInputElement | null = null;
+
+	// Trigger focus to the input field.
+	// useful when user click outside of the input (e.g icons)
+	// then this function will automatically make input focus.
+	function triggerInput() {
+		if (!inputRef) return;
+		inputRef.focus();
+	}
 </script>
 
 <div class="mx-2 flex items-center rounded bg-white px-3 shadow-md focus-within:shadow-lg {extend}">
-	<div class="mr-2">
+	<div class="mr-2 cursor-pointer" on:click={() => triggerInput()}>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			class="h-5 w-5"
@@ -24,6 +35,7 @@
 			type="text"
 			class="placeholder:text-slate-40 w-full py-3 outline-none placeholder:italic"
 			placeholder="Cari Kelas"
+			bind:this={inputRef}
 		/>
 	</div>
 </div>
