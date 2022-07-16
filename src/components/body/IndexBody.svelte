5a797d2ad6a1d15268b85a223febc78201b44ad1
resqiar <resqiageng@gmail.com>
2022-07-17 03:03:29 +0700

    refactor(indexbody): relocate see more into a different component

--- src/components/body/IndexBody.svelte
+++ src/components/body/IndexBody.svelte
@@ -3,6 +3,7 @@
 
 	import IndexHero from '../hero/IndexHero.svelte';
 	import IndexGridList from '../list/IndexGridList.svelte';
+	import SeeMoreNavigation from '../nav/SeeMoreNavigation.svelte';
 	import SearchInput from '../search/SearchInput.svelte';
 </script>
 
@@ -100,21 +101,7 @@
 			</h1>
 
 			<!-- SEE MORE NAVIGATION -->
-			<a href="#!" class="flex items-center gap-2 hover:underline">
-				<p class="hidden md:flex">Selengkapnya</p>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</a>
+			<SeeMoreNavigation />
 		</div>
 
 		<!-- GRID LIST -->
