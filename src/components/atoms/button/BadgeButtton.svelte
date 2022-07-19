5e93642856b1c35633596746c43693ef265aba5c
resqiar <resqiageng@gmail.com>
2022-07-20 01:18:53 +0700

    style(BadgeButton): change primary color to darker one

--- src/components/atoms/button/BadgeButtton.svelte
+++ src/components/atoms/button/BadgeButtton.svelte
@@ -5,14 +5,15 @@
 	 * When isSelected variabled change, the value below-
 	 * will also changes, think of it like useEffect in react.
 	 */
-	$: selectedBg = isSelected ? 'bg-purple-500' : 'bg-slate-200';
-	$: selectedHover = isSelected ? 'hover:bg-purple-400' : 'hover:bg-slate-300';
+	$: selectedBg = isSelected ? 'bg-primary' : 'bg-slate-200';
+	$: selectedHover = isSelected ? 'hover:bg-primary-hover' : 'hover:bg-slate-300';
 	$: isBold = isSelected ? 'font-medium' : 'font-normal';
+	$: textColor = isSelected ? 'text-white' : 'text-black';
 </script>
 
 <button
 	on:click
-	class="cursor-pointer whitespace-nowrap rounded px-4 py-1 ease-in-out hover:-translate-y-1 hover:shadow-lg hover:duration-150 {isBold} {selectedBg} {selectedHover}"
+	class="cursor-pointer whitespace-nowrap rounded px-4 py-1 ease-in-out hover:-translate-y-1 hover:shadow-lg hover:duration-150 {isBold} {selectedBg} {selectedHover} {textColor}"
 >
 	<slot />
 </button>
--- src/components/atoms/button/PrimaryButton.svelte
+++ src/components/atoms/button/PrimaryButton.svelte
@@ -5,7 +5,7 @@
 
 <button
 	type="button"
-	class="bg-primary m-0 rounded px-5 py-2.5 text-[14px] font-semibold leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-[#1d447e] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#163461] active:shadow-lg {extend}"
+	class="bg-primary active:bg-primary-active hover:bg-primary-hover m-0 rounded px-5 py-2.5 text-[14px] font-semibold leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg {extend}"
 	aria-label={ariaLabel}
 >
 	<slot />
