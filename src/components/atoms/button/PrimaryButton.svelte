338f9a4465a674f5dccd65a3397a051289129126
resqiar <resqiageng@gmail.com>
2022-07-20 01:14:27 +0700

    style(PrimaryButton): change primary color to darker one

--- src/components/atoms/button/PrimaryButton.svelte
+++ src/components/atoms/button/PrimaryButton.svelte
@@ -5,7 +5,7 @@
 
 <button
 	type="button"
-	class="m-0 rounded bg-blue-600 px-5 py-2.5 text-[14px] font-semibold leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg {extend}"
+	class="bg-primary m-0 rounded px-5 py-2.5 text-[14px] font-semibold leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-[#1d447e] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#163461] active:shadow-lg {extend}"
 	aria-label={ariaLabel}
 >
 	<slot />
